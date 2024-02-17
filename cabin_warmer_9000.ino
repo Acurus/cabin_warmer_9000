af8e3c493a82de8b402b870c9ad34f52f3a573f7
Jan Tore Kyrdalen <jtkyrdalen@gmail.com>
2024-02-17 21:52:11 +0100

    Ready for the cabin

--- cabin_warmer_9000.ino
+++ cabin_warmer_9000.ino
@@ -1,57 +1,87 @@
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
 #include "secrets.h"
 SoftwareSerial SIM7670Serial(5, 4);  // RX, TX
 
-const int ledPin = D2;  // Change this to the pin where your LED is connectedc:\Users\jtkyr\development\Arduino\cabin_warmer_9000\.gitignore
+const int txPin = D2;
+const int relayPin = 16;
 
-bool sendATCommand(const char* cmd, const char* expectedResponse, unsigned long timeout) {
+bool checkResponse(const String response, const char* expectedResponse) {
+  bool responseOK = false;
+  if (response.indexOf(expectedResponse) != -1) {
+    responseOK = true;
+  }
+
+  if (responseOK) {
+    Serial.println("Response OK");
+  } else {
+    Serial.println(response);
+    Serial.println("Timeout without expected Response");
+  }
+  return responseOK;
+}
+
+String sendATCommand(const char* cmd, unsigned long timeout) {
   Serial.print("Sending: ");
   Serial.print(cmd);
   Serial.println("...");
   SIM7670Serial.println(cmd);
   String response = "";
   unsigned long startTime = millis();
-  bool responseOK = false;
-
   while (millis() - startTime < timeout) {
     while (SIM7670Serial.available() > 0) {
       char c = SIM7670Serial.read();
       response += c;
     }
-
-    if (response.indexOf(expectedResponse) != -1) {
-      responseOK = true;
-      Serial.println(response);
-      break;  // found it
-    }
   }
-
-  Serial.print("Response was: ");
-  Serial.println(response);
-
-  if (responseOK)
-    Serial.println("Response OK");
-  else
-    Serial.println("Timeout without expected Response");
-  return responseOK;
+  return response;
 }
 
 
-bool processSms(const String& smsContent) {
+void processSms(const String& smsContent) {
   int start_number = smsContent.indexOf("+CMT: \"") + 7;
   int end_number = smsContent.indexOf("\"", start_number);
   String phoneNumber = smsContent.substring(start_number, end_number);
-
   int start_message = smsContent.lastIndexOf("\"") + 1;
   String messageText = smsContent.substring(start_message);
+  messageText.toLowerCase();
   messageText.trim();
   if (phoneNumberIsApproved(phoneNumber)) {
     Serial.print("Received SMS from: " + phoneNumber);
     Serial.println(" - " + messageText);
-    return true;
+    if (messageText == "på" || messageText == "pÅ") {  // Å is not lowered by toLowerCase()
+      heaterOn();
+      sendSMS(phoneNumber, "On signal sent");
+    } else if (messageText == "av") {
+      heaterOff();
+      sendSMS(phoneNumber, "Off signal sent");
+    } else if (messageText == "temp" || messageText == "temperatur") {
+      double temp = readTemperature();
+      sendSMS(phoneNumber, String(temp));
+    }
   }
-  return false;
 }
+
+
+void heaterOn() {
+  Serial.println("Turning heater on");
+  digitalWrite(relayPin, HIGH);
+  delay(3000);
+  digitalWrite(relayPin, LOW);
+}
+
+void heaterOff() {
+  Serial.println("Turning heater off");
+  digitalWrite(relayPin, HIGH);
+  delay(5000);
+  digitalWrite(relayPin, LOW);
+}
+
+double readTemperature() {
+  // Not implemented yet
+  return 20;
+}
+
 
 bool phoneNumberIsApproved(const String& phoneNumber) {
   int size = sizeof(approvedPhoneNumbers) / sizeof(approvedPhoneNumbers[0]);
@@ -67,30 +97,37 @@ void sendSMS(String number, String message) {
   String cmd = "AT+CMGS=\"" + number + "\"\r\n";
   SIM7670Serial.print(cmd);
   delay(100);
-  SIM7670Serial.println(message);
+  SIM7670Serial.print(message);
   delay(100);
   SIM7670Serial.write(0x1A);  // send ctrl-z
   delay(100);
 }
 
 void setup() {
+  // Set up serial
   SIM7670Serial.begin(115200);
   Serial.begin(115200);
 
-  pinMode(ledPin, OUTPUT);
-  sendATCommand("ATE0", "OK",1000);
-  bool boardIsUp = sendATCommand("AT", "OK", 1000);
+  // Turn off wifi to save power
+  WiFiMode(WIFI_STA);
+  WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);
+
+  // Set up pins
+  pinMode(txPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+
+  // Initialize A7670G
+  sendATCommand("ATE0", 1000);
+  bool boardIsUp = checkResponse(sendATCommand("AT", 1000), "OK");
   while (!boardIsUp) {
     delay(500);
     Serial.println("Retrying..");
-    boardIsUp = sendATCommand("AT", "OK", 1000);
+    boardIsUp = checkResponse(sendATCommand("AT", 1000), "OK");
   }
-
-  sendATCommand("AT+CMGF=1", "OK", 1000);          // set SMS format to text
-  sendATCommand("AT+CNMI=2,2,0,0,0", "OK", 1000);  // Enable receiving SMS notifications
+  sendATCommand("AT+CMGF=1", 1000);          // set SMS format to text
+  sendATCommand("AT+CNMI=2,2,0,0,0", 1000);  // Enable receiving SMS notifications
   Serial.println("Setup complete.");
-
-  sendSMS("+4797140645", "Hello from Arduino!");
 }
 
 void loop() {
